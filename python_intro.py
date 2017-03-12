19d7b0aabee2fa8244f6f9ead319e7b34e4ffc01
Martina Barb <martina48@gmail.com>
2017-03-12 14:20:06 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,22 @@
+# def hi():
+#     print("Hi there!")
+#     print("How are you?")
+# hi()
+# # #def hi(name):
+# #     if name == "Ola":
+# #         print("Hi Ola!")
+# #     elif name == "Sonja":
+# #         print("Hi Sonja!")
+# #     else:
+# #         print("Hi anonymous!")
+# # hi("Martina")
+# def hi1(name):
+#     print("Hi " + name + "!")
+# hi1("Rachel")
+# def hi2(name):
+#     print("Hi " + name + "!")
+# girls = "Rachel", "Monica", "Pheobe", "Ola", "You"
+# for name in girls:
+#     hi2(name)
+#     print('Next girl')
+# hi2(name)
