916d6a3f3f7807a0f19af79f850d084404f2bf09
Martina Barb <martina48@gmail.com>
2017-03-12 15:49:22 +0100

    s allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k_zj*_v@&br(7^v-c@$x6ua^tc1c@bs(pkz)=^5o&ijc&#ps^j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
